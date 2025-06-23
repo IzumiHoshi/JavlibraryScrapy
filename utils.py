144f6ab7bbde78df662f29f8e760d9b7c0ccdbed
izumihoshi <tsunami2deng@gmail.com>
2025-06-24 00:06:16 +0800

    feat: 增加poster处理

---
+++ utils.py
@@ -0,0 +1,43 @@
+# coding: utf-8 -*-
+from pathlib import Path
+from PIL import Image
+from typing import Tuple, List
+
+
+def split_poster_from_fanart(fanart_path: Path, poster_path: Path) -> None:
+    """
+    从 fanart 图片中分离出 poster 图片并保存
+    poster 图片位于 fanart 的右半部分,比例为5:7
+    """
+    fanart = Image.open(fanart_path)
+    width, height = fanart.size
+    poster_width = int(height * 5 / 7)
+    poster = fanart.crop((width - poster_width, 0, width, height))
+    poster.save(poster_path)
+    print(f"已保存 poster 图片：{poster_path.name}")
+
+
+def find_all_fanart_images(directory: Path) -> List[Path]:
+    """
+    查找指定目录下所有 fanart 图片，递归查找子目录
+    假设 fanart 图片以 '-fanart' 结尾
+    """
+    return list(directory.glob("**/*-fanart.*"))
+
+
+def process_all_fanarts(directory: Path) -> None:
+    """
+    处理指定目录下所有 fanart 图片，分离出 poster 图片
+    """
+    fanart_images = find_all_fanart_images(directory)
+    for fanart in fanart_images:
+        poster_path = fanart.with_name(
+            fanart.name.replace("-fanart", "-poster")
+        )  # 替换文件名
+        print(f"处理开始：{fanart.name} -> {poster_path.name}")
+        split_poster_from_fanart(fanart, poster_path)
+        print(f"处理完成：{fanart.name} -> {poster_path.name}")
+
+
+if __name__ == "__main__":
+    process_all_fanarts(Path(r"Y:\JAV\2025"))
